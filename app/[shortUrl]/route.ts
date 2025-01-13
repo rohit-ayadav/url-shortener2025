d112434328266757b2183f1fce24461b8529b8c2
Rohit kumar yadav <rohitkuyada@gmail.com>
2025-01-13 13:10:16 +0530

    Update route.ts

--- app/[shortUrl]/route.ts
+++ app/[shortUrl]/route.ts
@@ -1,16 +1,24 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: Request, context: { params: { shortUrl: string } }) {
+export async function GET(request: NextRequest) {
   try {
-    // Return the params and a test message as JSON
+    // Extract the path segments from the request URL
+    let paths = request.nextUrl.pathname.split("/");
+    paths = paths.filter((s) => s.length > 0);
+    console.log(paths);
+
+    // Get the last segment of the path (shortened URL)
+    const shortenURL = paths[paths.length - 1];
+
+    // Return the shortened URL and a test message
     return NextResponse.json({
       message: "Test successful",
-      params: context.params,
+      shortenURL: shortenURL,
     });
   } catch (error) {
     return NextResponse.json(
       {
-        message: "An error occurred during testing",
+        message: "An error occurred",
         error: error.message,
       },
       { status: 500 }
