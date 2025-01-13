7ca8bd00568c8bcc03e34f009adf366df9d25b5b
Rohit kumar yadav <rohitkuyada@gmail.com>
2025-01-13 13:04:43 +0530

    Update route.ts
    N

--- app/[shortUrl]/route.ts
+++ app/[shortUrl]/route.ts
@@ -1,11 +1,11 @@
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request, { params }: { params: { shortUrl: string } }) {
+export async function GET(request: Request, context: { params: { shortUrl: string } }) {
   try {
     // Return the params and a test message as JSON
     return NextResponse.json({
       message: "Test successful",
-      params: params,
+      params: context.params,
     });
   } catch (error) {
     return NextResponse.json(
